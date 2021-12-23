9ff68e6484ab67a88e50b5f65a1ab6625c46e579
YiZero <18350701710@163.com>
2021-12-23 21:55:20 +0800

    feat:add interceptors and configs

---
+++ src/main/java/com/njupt/system/config/InfoArgumentResolver.java
@@ -0,0 +1,32 @@
+package com.njupt.system.config;
+
+import com.njupt.system.model.Admin;
+import com.njupt.system.model.User;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * @author Elaine Huang
+ * @date 2021/12/21 9:25 AM
+ * @signature Do it while you can!
+ */
+@Component
+public class InfoArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        //只有 UserInfo
+        return methodParameter.getParameterType().isAssignableFrom(User.class)
+                || methodParameter.getParameterType().isAssignableFrom(Admin.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        if (TokenInterceptor.userHolder.get() == null)
+            return TokenInterceptor.adminHolder.get();
+        return TokenInterceptor.userHolder.get();
+    }
+}
---
+++ src/main/java/com/njupt/system/config/TokenInterceptor.java
@@ -0,0 +1,6 @@
+package com.njupt.system.config;/**
+ * @author Elaine Huang
+ * @date 2021/12/21 9:01 AM
+ * @signature Do it while you can!
+ */public class TokenInterceptor {
+}
